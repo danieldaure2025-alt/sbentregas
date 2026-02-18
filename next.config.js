74025c699d12cfd89e1f3ec075624aa210eef3cb
unknown <danieldaure2025@gmail.com>
2026-02-18 02:12:27 -0300

    fix: remover outputFileTracingRoot que causava path0/path0 no Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -17,3 +10,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
